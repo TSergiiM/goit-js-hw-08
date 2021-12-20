3acdfa9e0aefb6156df3867c5a3e8b8e2a0d91fe
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-20 22:13:27 +0200

    finish-task-1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,36 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
 console.log(galleryItems);
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+            <img class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>`;
+    })
+    .join('');
+}
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+const onGalleryClick = event => {
+  event.preventDefault();
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+  new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+};
+refs.gallery.addEventListener('click', onGalleryClick);
