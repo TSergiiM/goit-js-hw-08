4c27327a779c38265332b952c8a3944a5fc5872b
Sergii Tatsakovych <88043692+TSergiiM@users.noreply.github.com>
2021-12-21 01:01:43 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,16 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-const playedTime = 'videoplayer-current-time';
+
 const currentTime = function (event) {
-  localStorage.setItem(playedTime, event.seconds);
+  localStorage.setItem('videoplayer-current-time', event.seconds);
 };
 playerReload();
 player.on('timeupdate', throttle(currentTime, 1000));
 
 function playerReload() {
-  if (localStorage.getItem(playedTime)) {
-    player.setCurrentTime(localStorage.getItem(playedTime));
+  if (localStorage.getItem('videoplayer-current-time')) {
+    player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
   }
 }
